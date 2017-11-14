881eb06be4e77f9ce7593697848ad77a88a03cdc
Tyler Fischer <fischt77@uwosh.edu>
2017-11-13 21:07:24 -0600

    Created the client side structure for the start of our project

---
+++ Gaming-Wiki/ClientSide/home.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="keywords" content="gamingwiki index homepage signin signup " />
+<meta name="description" content="Gaming wiki homepage" />
+<title> GamingWiki</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+ 
+</head>
+<?php require_once('./initHeader.php') ?>
+<div id="main">
+     <?php require_once('./login/sign-up/signup.html') ?>
+     <?php require_once('./login/sign-in/signin.html') ?> 
+</div>
+<?php require_once('./initFooter.php') ?>
+<script src="./login/login.js"> </script>
+</html>
---
+++ Gaming-Wiki/ClientSide/initHeader.php
@@ -0,0 +1,23 @@
+
+<?php
+
+        echo "<header>
+			<a href='https://webdev.cs.uwosh.edu/students/guang16/gamingwikiindex/home.php'>
+			<img src='gamingwiki.png' alt='Gamingwiki Logo' width = '100' height = '100'/>
+			</a>
+    
+			<a href='https://www.wikipedia.org/'>
+			<img src='wikipedia.png' alt='wikipedia Logo'  width = '100' height = '100'/>
+			</a>
+    
+			</header>
+			<nav>
+    
+				<form action='search.php' method='post'>
+					<input type='text' name='search' id='search' class='inNav' col='2' placeholder='Custom Search'/>
+				</form>
+    
+				<input type='submit' value='SIGNIN'   class='inNav' id='signin'></input>
+				<input type='submit' value='SIGNUP' id='signup' class='inNav'></input>
+			</nav>"	
+?>
