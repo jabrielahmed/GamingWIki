50ed6e98a3b48870312b872e0bc766be8b916a44
guang16 <31829031+guang16@users.noreply.github.com>
2017-11-13 21:39:40 -0600

    Update articleCreator.php

--- Gaming-Wiki/ClientSide/articleCreator.php
+++ Gaming-Wiki/ClientSide/articleCreator.php
@@ -2,9 +2,8 @@
 <html lang="en">
 <head>
 <meta charset="utf-8"/>
-<meta name="keywords" content="gamingwiki index homepage signin signup " />
-<meta name="description" content="Gaming wiki homepage" />
 <title> GamingWiki</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
 <link rel="stylesheet" type="text/css" href="articleCreator.css" />
 <script src="articleCreator.js" type="text/javascript"></script>
 </head>
