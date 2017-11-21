60142f638c2992efe78c3d0b7b0374900aa486ae
Tyler Fischer <fischt77@uwosh.edu>
2017-11-20 20:00:05 -0600

    Update login.php

--- Gaming-Wiki/Server/Services/Login/login.php
+++ Gaming-Wiki/Server/Services/Login/login.php
@@ -1,11 +1,12 @@
 <?php
 require_once("../../DB/config.php");
     require_once("./login-service.php");
-    echo"login test";
+    $thing =phpversion();
+    echo"$thing";
     $db = new DB();
     $db->connect();
     $var = new UserTable($db);
-    print_r($_GET);
+    
     // $var->getTokenForUser($_GET['username'],$_GET['password']);
     $var->addUser($_GET['username'],$_GET['password']);
 ?>
