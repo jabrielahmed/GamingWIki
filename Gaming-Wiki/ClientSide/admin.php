c8a5296b5ec39dfcf413361479a6d0a2063f6acf
jabrielahmed <jabrielahmed96@gmail.com>
2017-11-26 22:18:34 -0600

    fixed minor changes

--- Gaming-Wiki/ClientSide/admin.php
+++ Gaming-Wiki/ClientSide/admin.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8"/>
-	<meta name="keywords" content="gamingwiki index homepage signin signup" />
-	<meta name="description" content="Gaming wiki homepage" />
+	<meta name="keywords" content="gamingwiki Admin Page" />
+	<meta name="description" content="Gaming wiki Admin Page" />
 	<title> GamingWiki</title>
 	<link rel="stylesheet" type="text/css" href="admin.css" />
 	<link rel="stylesheet" type="text/css" href="style.css" />
@@ -29,20 +29,21 @@
 				</td>
 				<td class="form" id="editgame">
 						<h1>Edit Game</h1>
+						<label> Game Name:</label>
 						<select>
 							<option name="game1">game1</option>
 							<option name="game2">game2</option>
 							<option name="game3">game3</option>
 							<option name="game4">game4</option>
-							<label> Game Name:</label>
 						</select>		
-						<label> Game Info:</label>	<textarea rows="5" cols="50">	</textarea>
+						<label> Game Info:</label>	
+						<textarea rows="5" cols="50">	</textarea>
 						<button>OK</button>
 				</td>
 				<td class="form" id="removegame">
 					<h1>Remove Game</h1>
 					<label> Game Name:</label>
-					<input type="Search" id="adminsearch" name="searchquestions" size="12" value=""/>
+					<input type="Search" id="adminsearch" name="searchquestions" size="12" />
 					<button>Remove</button>
 				</td>
 			</tr>
@@ -50,7 +51,7 @@
 				<td id="removes">
 					<h1>Add Game</h1>
 					<label> Game Name:</label>	
-					<input type="Search" id="adminsearch" name="searchquestions" size="12" value=""/>
+					<input type="Search" id="adminsearch" name="searchquestions" size="12" />
 					<label> Game Info:</label>
 					<textarea rows="5" cols="50"></textarea>
 					<button>OK</button>
@@ -58,13 +59,12 @@
 				<td id="removeaccount">
 					<h1>Remove Account</h1>
 					<label> Account Name:</label>
-					<input type="Search" id="adminsearch" name="searchquestions" size="12" value=""/>
+					<input type="Search" id="adminsearch" name="searchquestions" size="12" />
 					<button>Remove</button>
 				</td>
 			</tr>	
 		</table>
 	</div>
 	<?php require_once('./initFooter.php') ?>
-
  </body>
 </html>
