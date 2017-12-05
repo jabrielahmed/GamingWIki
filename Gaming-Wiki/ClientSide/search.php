ad60be28e2733fc210c1519a5af816ab8de23864
WendlandtC <31829521+WendlandtC@users.noreply.github.com>
2017-12-04 22:17:56 -0600

    Add files via upload

--- Gaming-Wiki/ClientSide/search.php
+++ Gaming-Wiki/ClientSide/search.php
@@ -20,7 +20,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8"/>
-<title> GamingWiki</title>
+<title>GamingWiki</title>
 <link rel="stylesheet" type="text/css" href="style.css" />
 <link rel="stylesheet" type="text/css" href="search.css" />
  
