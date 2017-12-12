f67a33414bf52f34efdc60645818d32dcc614f6f
guang16 <31829031+guang16@users.noreply.github.com>
2017-12-11 18:50:30 -0600

    demo change

--- Gaming-Wiki/ClientSide/account.php
+++ Gaming-Wiki/ClientSide/account.php
@@ -17,7 +17,7 @@ $article = $articleTable->getArticleByName($username);
 <html lang="en">
 <head>
 	<meta charset="utf-8"/>
-	<meta name="keywords" content="useraccount" />
+	<meta name="keywords" content="UserAccount" />
 	<meta name="description" content="Gaming wiki homepage" />
 	<title> GamingWiki</title>
 	<link rel="stylesheet" type="text/css" href="style.css" />
